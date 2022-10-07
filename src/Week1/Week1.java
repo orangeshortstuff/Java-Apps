2ccbec26f17862bec2e1043d319a9d86b4e53efd
orangeshortstuff <22206619@bucks.ac.uk>
2022-10-07 10:15:47 +0100

    put it all into functions

--- src/Week1/Week1.java
+++ src/Week1/Week1.java
@@ -17,31 +17,63 @@ public class Week1
         System.out.println(" by Nicholas Day and Derek Peacock");
         System.out.println();
 
-        String name = InputReader.getString("Enter your name > ");
-        System.out.println("Hello " + name);
+        enterName();
+        areaOfRectangle();
+        averageAndReverse();
+        birthYearFromAge();
+        milesFromFeet();
+        interestOnDeposit();
 
-        int width = InputReader.getInt("Enter the width > ");
-        int height = InputReader.getInt("Enter the height > ");
-        System.out.println(width * height);
+    }
 
+    private static void milesFromFeet()
+    {
+        float feet = InputReader.getInt("How many feet? ");
+        float miles = feet / 5280;
+        System.out.println(feet + " feet is " + miles + " miles");
+        System.out.println("");
+    }
+
+    private static void birthYearFromAge()
+    {
+        int age = InputReader.getInt("Enter your age: ");
+        System.out.println("Were you born in " + (2022 - age) + "?");
+        System.out.println("");
+    }
+
+    private static void averageAndReverse() 
+    {
         int first = InputReader.getInt("Enter the first number > ");
         int second = InputReader.getInt("Enter the second number > ");
         float avg = (first + second);
         avg /= 2; 
         System.out.println(avg);
-        
+
         System.out.println("Normal order: " + first + " " + second);
         System.out.println("Reverse order: " + second + " " + first);
+        System.out.println("");
+    }
 
-        int age = InputReader.getInt("Enter your age: ");
-        System.out.println("Were you born in " + (2022 - age) + "?");
+    private static void areaOfRectangle()
+    {
+        int width = InputReader.getInt("Enter the width > ");
+        int height = InputReader.getInt("Enter the height > ");
+        System.out.println(width * height);
+        System.out.println("");
+    }
 
-        float feet = InputReader.getInt("How many feet? ");
-        float miles = feet / 5280;
-        System.out.println(feet + " feet is " + miles + " miles");
+    private static void enterName()
+    {
+        String name = InputReader.getString("Enter your name > ");
+        System.out.println("Hello " + name);
+        System.out.println("");
+    }
 
+    private static void interestOnDeposit()
+    {
         float deposit = InputReader.getInt("Enter your deposit: ");
         deposit *= 1.01;
         System.out.println("You will have " + deposit + " pounds at the end of the year");
-    } 
+        System.out.println("");
+    }
 }
