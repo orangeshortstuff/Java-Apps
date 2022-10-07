b0c560310a4aa37fa4edf955f01dd3dbf14b5cc4
orangeshortstuff <22206619@bucks.ac.uk>
2022-10-07 10:41:19 +0100

    7 times table and grade conversion tasks
    from the practical session

--- src/Week2/Week02.java
+++ src/Week2/Week02.java
@@ -1,8 +1,11 @@
 package Week2;
 
+import helpers.InputReader;
+
 public class Week02 
 {
     public static final String CONSOLE_CLEAR = "\033[H\033[2J";
+    public static final int SEVEN = 7;
     public static void main(String[] args)
     {
         System.out.println(CONSOLE_CLEAR);
@@ -15,5 +18,58 @@ public class Week02
         System.out.println();
         System.out.println(" by Nicholas Day and Derek Peacock");
         System.out.println();
+
+        do7TimesTable();
+        doConvertGrade();
+    }
+
+    private static void doConvertGrade()
+    {
+        String grade = InputReader.getString("Please enter your grade > ");
+        char letter = grade.charAt(0);
+
+        if(letter == 'A')
+        {
+            System.out.println("Your grade is First Class!");
+        }
+
+        else if(letter == 'B')
+        {
+            System.out.println("Your grade is Upper Second Class");
+        }
+
+        else if(letter == 'C')
+        {
+            System.out.println("Your grade is Lower Second Class");
+        }
+
+        else if(letter == 'D')
+        {
+            System.out.println("Your grade is Third Class");
+        }
+
+        else if(letter == 'E')
+        {
+            System.out.println("Your grade is Ordinary Class");
+        }
+
+        else if(letter == 'F')
+        {
+            System.out.println("You Failed");
+        }
+        else {
+            doConvertGrade();
+        }
+
+    }
+
+    private static void do7TimesTable()
+    {
+        int product; int multiple;
+        for (multiple = 1; multiple <= 12; multiple++)
+        {
+            product = multiple * SEVEN;
+            System.out.println(multiple + " x " + SEVEN + " = " + product);
+        }
     }     
 }
